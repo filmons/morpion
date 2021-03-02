e854d2edb489f2b7ea22407bba228b4fff1f81ee
seare <73501892+filmons@users.noreply.github.com>
2021-03-02 15:08:37 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// cible ou recuper tout les element pour manupule avec DOM
+
+
+
+
+
+
+
+
+
+// on commence addEventListener pour chaque element
+
+
+// on reece 8 tablo
+
+
+
+
+
+// on determin les juer
+
+
+
+
+
+
+
+// la position du tablot
+
+
+
+
+
+
+
