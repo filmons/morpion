646206315d097fef77b0a91bbb557c862dae11a2
Your Name <filmonseare@gmil.com>
2021-03-02 15:38:25 +0100

    the first work for find element of game morpion

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-// cible ou recuper tout les element pour manupule avec DOM
+// Cible les éléments dans HTML
+const buttons = document.querySelectorAll("#game>div>button");
+const gameStatus = document.querySelector("#game_status");
+
+// Assign les joueurs
+let joueurX = [];
+let joueurO = [];
+let joueurActuel = "o";
+
+// Affiche les symboles "X" ou "O" sur chaque bouton
+for (let i = 0; i<buttons.length; i++){
+    buttons[i].addEventListener("click", function(e){
+        // console.log(i)
+        if (joueurActuel == "o"){
+            joueurActuel = "x";
+            e.target.textContent = "O"
+            joueurX.push(i)
+            console.log(joueurX)
+            gameStatus.textContent = "Joueur X c'est votre tour."
+            // console.log(e.target)
+            // console.log(joueurActuel);
+            // console.log("Joueur x c'est votre tour")
+        } else {
+            joueurActuel = "o";
+            e.target.textContent = "X"
+            joueurO.push(i)
+            console.log(joueurO)
+            gameStatus.textContent = "Joueur O c'est votre tour."
+            // console.log("Jour o c'est votre tour")
+        }
+    }, {once: true}) // Evite l'affichage doublée sur chaque bouton
+}
+
+// Assigne la combinaison de gain
 
 
-
-
-
-
-
-
-
-// on commence addEventListener pour chaque element
-
-
-// on reece 8 tablo
-
-
-
-
-
-// on determin les juer
-
-
-
-
-
-
-
-// la position du tablot
 
 
 
